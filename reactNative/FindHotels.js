3b017aaf70baae3b2fbd40011d0b549f17634e86
breezebot <breezeblocksfsa@gmail.com>
2015-08-31 17:02:09 -0400

    Exported BreezeBlocks Project

--- reactNative/FindHotels.js
+++ reactNative/FindHotels.js
@@ -29,11 +29,11 @@ module.exports = React.createClass({
             </View>
             <View style={[styles.view3]}>
                 <MapView
-                	latitude={40.704814} longitude={-74.008930} latitudeDelta={undefined} longitudeDelta={undefined} mapType={'standard'}
+                	latitude={40.704814} longitude={-74.008930} latitudeDelta={0.001} longitudeDelta={0.001} mapType={'standard'}
                 	style={[styles.view3Mapview4]}>
                 </MapView>
             </View>
-            <View >
+            <View>
                 <ScrollView 
                 dataSource={['http://downtownmagazinenyc.com/wp-content/uploads/2012/04/AndazWallStreetHotelNYC091610-01-LO-TPP.jpeg', 'http://www.ritzcarlton.com/NR/rdonlyres/D04FD033-3DC9-48BA-B564-80A8CC74E95C/0/Ritz_BatteryPark_00099_1220x520.jpg', 'http://ihg.scene7.com/is/image/ihg/holiday-inn-new-york-3442552674-4x3']}
                 style={[styles.view5Scrollview6]}>
@@ -61,18 +61,19 @@ var styles = StyleSheet.create({
       view1Navbar2:{
           paddingLeft: 0,
           paddingRight: 0,
-          paddingTop: 11,
-          paddingBottom: 0,
+          paddingTop: 32,
+          paddingBottom: 21,
           marginLeft: 0,
           marginRight: 0,
           marginTop: 0,
           marginBottom: 0,
           fontSize: 17,
+          fontWeight: 'bold',
           textAlign: 'center',
-          backgroundColor: '#c82c2f',
           color: '#ffffff',
+          backgroundColor: '#9c1c00',
           width: 375,
-          height: 85,
+          height: 73.75,
       }, 
       view3:{
       }, 
@@ -86,9 +87,9 @@ var styles = StyleSheet.create({
           marginTop: 0,
           marginBottom: 0,
           width: 375,
-          height: NaN,
+          height: 265,
       }, 
-      view5Scrollview6:{
+      view2Scrollview6:{
           paddingLeft: 0,
           paddingRight: 0,
           paddingTop: 0,
@@ -98,40 +99,46 @@ var styles = StyleSheet.create({
           marginTop: 0,
           marginBottom: 0,
           width: 375,
+          height: 238.75,
       }, 
+/*      view5Scrollview6:{
+          width: 375,
+          height: 122,
+          position: 'relative',
+      },*/ 
       view7:{
           flexDirection: 'row',
       }, 
       view8:{
-          height: NaN,
+          height: 200,
       }, 
       view7Listview9:{
           paddingLeft: 0,
           paddingRight: 0,
           paddingTop: 0,
           paddingBottom: 0,
-          marginLeft: 0,
+          marginLeft: 40,
           marginRight: 0,
           marginTop: 0,
           marginBottom: 0,
           fontSize: 15,
           textAlign: 'center',
           width: 234.375,
-          height: NaN,
+          height: 122,
       }, 
       view7Listview10:{
           paddingLeft: 0,
           paddingRight: 0,
           paddingTop: 0,
           paddingBottom: 0,
-          marginLeft: 0,
+          marginLeft: 30,
           marginRight: 0,
           marginTop: 0,
           marginBottom: 0,
           fontSize: 15,
           textAlign: 'center',
           width: 234.375,
-          height: NaN,
+          //height: 122,
       }, 
       view8Tabbarios11:{
           paddingLeft: 0,
