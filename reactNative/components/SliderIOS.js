0f057151b4cfdbb432f3dfc686d5ee4286a3fe47
breezebot <breezeblocksfsa@gmail.com>
2015-08-31 17:24:06 -0400

    Exported BreezeBlocks Project

--- reactNative/components/SliderIOS.js
+++ reactNative/components/SliderIOS.js
@@ -18,6 +18,9 @@ module.exports = React.createClass({
   render() {
     return (
       <View>
+        <Text style={styles.text} >
+          ${Math.round(this.state.value*1000)}
+        </Text>
         <SliderIOS
           style={[this.props.style, styles.slider]}
           onValueChange={(value) => this.setState({value: value})} />
@@ -28,6 +31,12 @@ module.exports = React.createClass({
 
 var styles = StyleSheet.create({
   slider: {
+    margin: 10,
+  },
+  text: {
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: '500',
     margin: 10,
   },
 });
